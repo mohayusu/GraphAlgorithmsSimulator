9b6712cf2e241993a6620bff43816adbddcd92ee
Mohamed Mohamed <mohamedyu98@gmail.com>
2018-08-04 14:33:49 -0400

    Added a tab bar and buttons appear on graph when the user taps the screen

--- GraphAlgorithmsSimulator/GraphViewController.swift
+++ GraphAlgorithmsSimulator/GraphViewController.swift
@@ -10,17 +10,33 @@ import UIKit
 
 class GraphViewController: UIViewController {
 
+    @IBOutlet weak var graphViewOutlet: Graph!
+    
+    @IBOutlet weak var linesView: UIView!
+    
+    @IBAction func graphTapped(_ sender: UITapGestureRecognizer) {
+        var location = sender.location(in: view)
+        let button = UIButton(frame: CGRect(x: location.x, y: location.y, width: 15, height: 15))
+        button.layer.cornerRadius = 0.5 * button.bounds.size.width
+        button.clipsToBounds = true
+        button.backgroundColor = .blue
+        print(location)
+        location.x = graphViewOutlet.bounds.width / 2
+        location.y = graphViewOutlet.bounds.height / 2
+        print(location)
+        self.view.addSubview(button)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         UIApplication.shared.statusBarStyle = .lightContent
+        linesView.backgroundColor = UIColor.white.withAlphaComponent(0.5)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
---
+++ GraphAlgorithmsSimulator/Line.swift
@@ -0,0 +1,33 @@
+//
+//  Line.swift
+//  GraphAlgorithmsSimulator
+//
+//  Created by Mohamed Mohamed on 8/4/18.
+//  Copyright © 2018 Mohamed Mohamed. All rights reserved.
+//
+
+import UIKit
+
+class Line: UIView {
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    var line = UIBezierPath()
+    var points: [CGPoint] = []
+    
+    func addPoint() {
+        
+    }
+    
+    override func draw(_ rect: CGRect) {
+     /*   var currX: CGFloat = 0
+        var currY: CGFloat = 0
+        
+        // draws vertical line in the middle of the screen
+        line.move(to: .init(x: 0, y: 0))
+        line.addLine(to: .init(x: bounds.width, y: bounds.height))
+        UIColor.black.setStroke()
+        line.lineWidth = 1.5
+        line.stroke()*/
+    }
+
+}
